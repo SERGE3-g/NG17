3c2cb62ee76365fbc845b738bce5d0dd8989d1fe
SERGE GUEA <113424842+SERGE3-g@users.noreply.github.com>
2023-12-16 13:43:27 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule,]
 })
 export class AppComponent {
   title = 'NG17';
+  brand: any;
 }
